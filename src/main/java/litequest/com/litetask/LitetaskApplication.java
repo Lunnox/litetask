3c0bf2c27061338bad4f023e9de62b3b0fd94a91
lunnox <fire.lunnox@gmail.com>
2019-09-28 13:20:56 +0300

    v 0.1

---
+++ src/main/java/litequest/com/litetask/LitetaskApplication.java
@@ -0,0 +1,13 @@
+package litequest.com.litetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitetaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LitetaskApplication.class, args);
+	}
+
+}
---
+++ src/main/java/litequest/com/litetask/controller/MainController.java
@@ -0,0 +1,9 @@
+package litequest.com.litetask.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+}
